d6acc72208e2deeac155a503fed2f422b0f892dd
Eric Thomas <reactomoss@gmail.com>
2022-09-03 03:57:50 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 cd ligo/src/.build
 
 tezos-client originate contract Tezrun transferring 0.0001 from tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq \
-running multi_asset_main.tz \
+running tezrun.tz \
 --force \
 --init '
-(Pair (Pair (Pair "tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq" False) None)
-      (Pair 0 "1970-01-01T00:00:59Z")
-      0
+(Pair (Pair (Pair (Pair "tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq" False) None) {})
+      (Pair (Pair 0 0) "1970-01-01T00:00:59Z" 0)
       0)
 ' \
 --burn-cap 1.1165
