61e9d74860b748c05a4a31f337424250e1882415
Eric Thomas <reactomoss@gmail.com>
2022-09-26 04:15:57 -0400

    update jsligo

--- build.sh
+++ build.sh
@@ -2,23 +2,23 @@ cd ligo/src
 rm -rf .build
 mkdir -p .build
 
-ligo compile contract tezrun.mligo --entry-point main  > ./.build/tezrun.tz
+ligo compile contract tezrun.jsligo --entry-point main  > ./.build/tezrun.tz
 
-ligo compile storage tezrun.mligo --entry-point main '
+ligo compile storage tezrun.jsligo --entry-point main '
 {
-  admin = {
-    admin = ("tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq" : address);
-    pending_admin = (None : address option);
-    paused = false;
-  };
-  race = {
-    race_id = 0n;
-    status = 0n;
-    winner = 0n;
-    ready_time = 0n;
-    start_time = Tezos.get_now ();
-    bettings = ([] : betting_list);
-    rewards = (Big_map.empty : reward_ledger);
+  admin: {
+    admin: ("tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq" as address),
+    pending_admin: None() as option<address>,
+    paused: false,
+  },
+  race: {
+    race_id: 0 as nat;
+    status: 0 as nat;
+    winner: 0 as nat;
+    ready_time: 0 as nat;
+    start_time: Tezos.get_now();
+    bettings: ([] as Betting.betting_list);
+    rewards: (Big_map.empty as Betting.reward_ledger);
   };
 }
 ' > ./.build/tezrun_storage.tz
