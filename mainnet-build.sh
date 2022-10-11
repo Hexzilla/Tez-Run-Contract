1a98e72aa6b6de2aeac6f0418c9ba287cf803d97
Eric Thomas <reactomoss@gmail.com>
2022-10-11 04:49:00 -0400

    mainnet scripts

---
+++ mainnet-build.sh
@@ -0,0 +1,28 @@
+cd ligo/src
+rm -rf .build
+mkdir -p .build
+
+ligo compile contract tezrun.jsligo --entry-point main  > ./.build/tezrun.tz
+
+ligo compile storage tezrun.jsligo --entry-point main '
+{
+  admin: {
+    admin: ("tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq" as address),
+    pending_admin: None() as option<address>,
+    paused: false,
+  },
+  race: {
+    race_id: 0 as nat,
+    status: 0 as nat,
+    winner: 0 as nat,
+    ready_time: 300 as int,
+    start_time: Tezos.get_now(),
+    bets: list([]) as Types.ticket_list,
+    rewards: Big_map.empty as Types.reward_ledger
+  },
+  tokens: {
+    uusd: None() as option<address>,
+  },
+}
+' > ./.build/tezrun_storage.tz
+
