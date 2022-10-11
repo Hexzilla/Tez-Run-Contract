6f782b93736a64c125f336feaf218cd4e7dc7d79
Eric Thomas <reactomoss@gmail.com>
2022-10-11 05:39:25 -0400

    update storage structure

--- build.sh
+++ build.sh
@@ -6,23 +6,19 @@ ligo compile contract tezrun.jsligo --entry-point main  > ./.build/tezrun.tz
 
 ligo compile storage tezrun.jsligo --entry-point main '
 {
-  admin: {
-    admin: ("tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq" as address),
-    pending_admin: None() as option<address>,
-    paused: false,
-  },
-  race: {
-    race_id: 0 as nat,
-    status: 0 as nat,
-    winner: 0 as nat,
-    ready_time: 300 as int,
-    start_time: Tezos.get_now(),
-    bets: list([]) as Types.ticket_list,
-    rewards: Big_map.empty as Types.reward_ledger
-  },
-  tokens: {
-    uusd: None() as option<address>,
-  },
+  admin: ("tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq" as address),
+  pending_admin: None() as option<address>,
+  paused: false,
+
+  race_id: 0 as nat,
+  status: 0 as nat,
+  winner: 0 as nat,
+  ready_time: 300 as int,
+  start_time: Tezos.get_now(),
+  bets: list([]) as Types.ticket_list,
+  rewards: Big_map.empty as Types.reward_ledger,
+
+  uusd: None() as option<address>,
 }
 ' > ./.build/tezrun_storage.tz
 
--- mainnet-build.sh
+++ mainnet-build.sh
@@ -6,23 +6,19 @@ ligo compile contract tezrun.jsligo --entry-point main  > ./.build/tezrun.tz
 
 ligo compile storage tezrun.jsligo --entry-point main '
 {
-  admin: {
     admin: ("tz1bxwduvRwBhq59FmThGKD5ceDFadr57JTq" as address),
     pending_admin: None() as option<address>,
     paused: false,
-  },
-  race: {
+    
     race_id: 0 as nat,
     status: 0 as nat,
     winner: 0 as nat,
     ready_time: 300 as int,
     start_time: Tezos.get_now(),
     bets: list([]) as Types.ticket_list,
-    rewards: Big_map.empty as Types.reward_ledger
-  },
-  tokens: {
+    rewards: Big_map.empty as Types.reward_ledger,
+    
     uusd: Some("KT1XRPEPXbZK25r3Htzp2o1x7xdMMmfocKNW" as address),
-  },
 }
 ' > ./.build/tezrun_storage.tz
 
